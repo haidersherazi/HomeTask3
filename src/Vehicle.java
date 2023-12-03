0a19097932b4c09467f92983860aa0854bfe9e4f
Haider Ali <syedhaidersherazi38@gmail.com>
2023-12-03 14:50:30 +0100

    created Vehicle class with the id and current arrival time calculation of the vehicle

---
+++ src/Vehicle.java
@@ -0,0 +1,27 @@
+
+class Vehicle {
+	private int id;
+	private long arrivalTime;
+
+    public Vehicle(int id) {
+        this.arrivalTime = System.currentTimeMillis();
+        this.id = id;
+    }
+
+    public long getArrivalTime() {
+        return arrivalTime;
+    }
+    
+    public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setArrivalTime(long arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+
+}
